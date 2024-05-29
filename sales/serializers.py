ca5e4511e9d58dff3e1a3f42377c290e787ae14d
Gary H <garyy.hd.123@gmail.com>
2024-05-29 19:48:46 +0700

    get sales

--- sales/serializers.py
+++ sales/serializers.py
@@ -9,11 +9,13 @@ class SaleDetailsSerializer(serializers.ModelSerializer):
 
 class SaleDetailSerializer(serializers.ModelSerializer):
     note = serializers.CharField(required=False, allow_null=True, allow_blank=True)  # Optional field
+    product_category_id = serializers.IntegerField(required=False, allow_null=True)  # Optional field
+    product_name = serializers.CharField(required=False, allow_null=True, allow_blank=True)  # Optional field
 
     class Meta:
         model = SaleDetails
         # Exclude sale_id, product_name, and product_category_id
-        fields = ['product_id', 'sold_for', 'quantity', 'note']
+        fields = ['product_id', 'product_category_id', 'product_name', 'sold_for', 'quantity', 'note']
 
     def create(self, validated_data):
         product_id = validated_data['product_id']
@@ -30,13 +32,35 @@ class SaleDetailSerializer(serializers.ModelSerializer):
 
         return super().create(validated_data)
 
+    def update(self, instance, validated_data):
+        product_id = validated_data.get('product_id', instance.product_id)
+
+        try:
+            product = Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            raise serializers.ValidationError(f"Product with id {product_id} does not exist.")
+
+        instance.product_id = product_id
+        instance.product_name = product.name
+        instance.product_category_id = product.category_id
+        instance.sold_for = validated_data.get('sold_for', instance.sold_for)
+        instance.quantity = validated_data.get('quantity', instance.quantity)
+        instance.note = validated_data.get('note', instance.note)
+
+        instance.save()
+        return instance
+
 
 class SaleSerializer(serializers.ModelSerializer):
     details = SaleDetailSerializer(many=True)
+    sale_no = serializers.CharField(required=False, allow_null=True, allow_blank=True)  # Optional field
+    paid = serializers.IntegerField(required=False, allow_null=True)  # Optional field
+    change = serializers.IntegerField(required=False, allow_null=True)  # Optional field
+    total_amount = serializers.IntegerField(required=False, allow_null=True)  # Optional field
 
     class Meta:
         model = Sale
-        fields = ['id', 'date', 'payment_method', 'details']
+        fields = ['id', 'sale_no', 'date', 'paid', 'payment_method', 'change', 'total_amount', 'details']
 
     def create(self, validated_data):
         details_data = validated_data.pop('details')
@@ -45,3 +69,16 @@ class SaleSerializer(serializers.ModelSerializer):
             detail_data['sale_id'] = sale.id
             SaleDetailSerializer().create(detail_data)
         return sale
+
+
+class GetSaleSerializer(serializers.ModelSerializer):
+    details = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Sale
+        fields = ['id', 'sale_no', 'date', 'paid', 'payment_method', 'change', 'total_amount', 'details']
+
+    def get_details(self,obj):
+        sale_details = SaleDetails.objects.filter(sale_id=obj.id)
+        return SaleDetailSerializer(sale_details, many=True).data
+
--- sales/views.py
+++ sales/views.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
-from sales.serializers import SaleSerializer
+from sales.serializers import SaleSerializer, GetSaleSerializer
 from pbo_uas.models import Sale
 from pbo_uas.response import ok_with_msg, ok_with_data, error_with_msg
 
@@ -8,9 +8,7 @@ from pbo_uas.response import ok_with_msg, ok_with_data, error_with_msg
 def getSale(request):
     # get all the data from db
     sales = Sale.objects.all()
-
-    # serialize and return it
-    serializer = SaleSerializer(sales, many=True)
+    serializer = GetSaleSerializer(sales, many=True)
     return ok_with_data(data=serializer.data, msg='ok')
 
 
